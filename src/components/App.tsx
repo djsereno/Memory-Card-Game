c4c4d370a6ee4a15286503a8b6b9b3b39dc0a0ea
Derek Sereno <djsereno91@gmail.com>
2024-09-21 15:13:52 -0700

    Add basic html and css

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -8,26 +8,30 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="container">
+      <header>
+        <h1>Memory Card Game</h1>
+        <div className="scoreboard">
+          <div className="score">
+            <p>Score: <span id="current-score">0</span></p>
+          </div>
+          <div className="high-score">
+            <p>High Score: <span id="high-score">0</span></p>
+          </div>
+        </div>
+      </header>
+
+      <section className="game-board">
+        <div className="card" data-id="1"></div>
+        <div className="card" data-id="2"></div>
+        <div className="card" data-id="3"></div>
+        <div className="card" data-id="4"></div>
+        <div className="card" data-id="5"></div>
+        <div className="card" data-id="6"></div>
+        <div className="card" data-id="7"></div>
+        <div className="card" data-id="8"></div>
+      </section>
+    </div>
     </>
   )
 }
