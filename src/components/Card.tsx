f73a2246c77d7c5d343d293622bacb0df435259a
Derek Sereno <djsereno91@gmail.com>
2024-10-07 23:57:52 -0700

    Add some card flipping styles

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 
+import cardback from '../assets/cardback.png';
+
 interface CardProps {
   id: number;
   imageUrl: string;
@@ -17,20 +19,32 @@ const Card: React.FC<CardProps> = ({ id, imageUrl, onClick }: CardProps) => {
     setIsAnimated(false);
   };
 
-  const className = 'card'
+  const className = 'flip-card'
     .concat(id === -1 ? ' flipped' : ' shine')
     .concat(isAnimated ? ' animated' : '');
 
   const style = id !== -1 ? { backgroundImage: `url(${imageUrl})` } : undefined;
 
   return (
-    <div
-      className={className}
-      style={style}
-      onClick={onClick}
-      onMouseEnter={handleMouseEnter}
-      onAnimationEnd={handleAnimationEnd}>
-      {id}
+    // <div
+    //   className={className}
+    //   style={style}
+    //   onClick={onClick}
+    //   onMouseEnter={handleMouseEnter}
+    //   onAnimationEnd={handleAnimationEnd}>
+    //   {id}
+    // </div>
+
+    <div className="flip-card">
+      <div className="flip-card-inner">
+        <div className="flip-card-back">
+          <img src={cardback} />
+        </div>
+        <div className="flip-card-front">
+          <img src={imageUrl} />
+          <span>{id}</span>
+        </div>
+      </div>
     </div>
   );
 };
