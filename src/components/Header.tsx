083313ae8b0abba4dc76ce5957e2fe89b12ddd32
Derek Sereno <djsereno91@gmail.com>
2024-10-13 11:35:40 -0700

    Header and background pattern style updates

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
 import logo from '../assets/logo.png';
+import ball from '../assets/loading.png';
 import '../styles/Header.scss';
 
 interface HeaderProps {
@@ -9,14 +10,20 @@ interface HeaderProps {
 const Header = ({ currentScore, highScore }: HeaderProps) => {
   return (
     <header className="header">
-      <span className="instructions">
-        <p>Pick a card, but don't pick</p>
-        <p>the same card twice!</p>
-      </span>
-      <img className="logo" src={logo} alt="logo" />
-      <div className="score-board">
-        <p className="high-score">{`High Score: ${highScore}`}</p>
-        <p className="current-score">{`Score: ${currentScore}`}</p>
+      <div className="header__instructions">
+        <img className="accent" src={ball} alt="accent" />
+        <div className="instructions">
+          <p>Pick a card, but don't pick</p>
+          <p>the same card twice!</p>
+        </div>
+      </div>
+      <img className="header__logo" src={logo} alt="logo" />
+      <div className="header__scoreboard">
+        <div className="scoreboard">
+          <p className="high-score">{`High Score: ${highScore}`}</p>
+          <p className="current-score">{`Score: ${currentScore}`}</p>
+        </div>
+        <img className="accent" src={ball} alt="accent" />
       </div>
     </header>
   );
